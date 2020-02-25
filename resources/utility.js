a019137fcd389f74b9b0da7cf056a44d891aa30c
SANTIAGO JIMENEZ RIOS <santiago.jimenez_rio@uao.edu.co>
2020-02-24 19:33:11 -0500

    Terminado

--- resources/utility.js
+++ resources/utility.js
@@ -1,20 +1,38 @@
-var nuNum1;
-var nuNum2;
-var nuOp;
+var nuNum1 = 0;
+var nuNum2 = 0;
+var stSelected = "suma";
 
-function OpMatematica(){
-    var nuResultado;
-    if(nuTipoOp == 1){
-        nuResultado=nuNum1+nuNum2;
+function OpMat(){
+    var nuResult;
+    if(stSelected == "suma"){
+        nuResult=nuNum1+nuNum2;
     }
-    else if(nuTipoOp == 2){
-        nuResultado=nuNum1-nuNum2;
+    else if(stSelected == "resta"){
+        nuResult=nuNum1-nuNum2;
     }
-    else if(nuTipoOp == 3){
-        nuResultado=nuNum1*nuNum2;
+    else if(stSelected == "multiplicacion"){
+        nuResult=nuNum1*nuNum2;
     }
-    else if(nuTipoOp == 4){
-        nuResultado=nuNum1/nuNum2;
+    else if(stSelected == "division"){
+        nuResult=nuNum1/nuNum2;
     }
-    return nuResultado;
+    return nuResult;
+}
+
+function handleInput(stInput){
+    if(stInput=="inputNum1"){
+        nuNum1=parseFloat(document.getElementById(stInput).value);
+    }
+    else{
+        nuNum2=parseFloat(document.getElementById(stInput).value);
+    }
+    document.getElementById("txtResult").innerHTML=(OpMat()).toFixed(2);
+}
+
+
+
+function selectOp(){
+    var e = document.getElementById("stSelected");
+    stSelected = e.options[e.selectedIndex].value;
+    document.getElementById("txtResult").innerHTML=(OpMat()).toFixed(2);
 }
